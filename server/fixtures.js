ffc8fc53874a062108953447c24a09000bc6aad4
Silver Chen <silver.chen@gmail.com>
2013-11-06 03:00:26 +0000

    Wire collection into postsList template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,16 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
-    title: 'Introducing Telescope',
+    title: 'Introducing Telescope 2',
     author: 'Sacha Greif',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
   Posts.insert({
-    title: 'Meteor',
+    title: 'Meteor 2',
     author: 'Tom Coleman',
     url: 'http://meteor.com'
   });
   Posts.insert({
-    title: 'The Meteor Book',
+    title: 'The Meteor Book 2',
     author: 'Tom Coleman',
     url: 'http://themeteorbook.com'
   });
