686b37534cfbe0c90ce85fd3f81c124f8229d652
Silver Chen <silver.chen@gmail.com>
2013-12-01 13:27:32 +0000

    denormalized the numbers of comments into the post

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -6,9 +6,6 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  },
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count(); 
   }
   
 });
